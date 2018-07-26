b646de9b5283e667a39320e12cec6bdb0707f8c7
taryn yaeger <tsyaeger@gmail.com>
2018-07-26 22:10:01 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    if @post.new(post_params).valid?
+    if Post.new(post_params).valid?
       @post.update(post_params)
       redirect_to post_path(@post)
     else 
