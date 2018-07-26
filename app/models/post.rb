54920a8b1e97b487310ca732a05fb312b0238121
taryn yaeger <tsyaeger@gmail.com>
2018-07-26 22:02:14 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :category, includes: ["Fiction", "Non-Fiction"]
+  validates :category, excludes: [nil]
+  validates :content, length: { minimum: 100 }
+  
 end
